4c592be97cfca3247f2b3c7fd131454f626afd1f
0Kate <o.keito317@gmail.com>
2020-06-28 12:46:31 +0900

    add packages to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 setup(
     name='kubic',
     version='0.1.0',
     description='kubectl interactive console',
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'kubic = kubic.kubic:main',
