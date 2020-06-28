6c47194ebaaf87c63e2d71a75a5b5b6ca0eb32b9
0Kate <o.keito317@gmail.com>
2020-06-28 12:30:25 +0900

    add entry_point to setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,9 @@ setup(
     name='kubic',
     version='0.1.0',
     description='kubectl interactive console',
+    entry_points={
+        'console_scripts': [
+            'kubic = kubic.kubic:main',
+        ]
+    }
 )
