962914922b3dbaf0fe5f954a0c464a516f7293df
0Kate <o.keito317@gmail.com>
2020-06-30 00:24:01 +0900

    implement executor

--- kubic/executor.py
+++ kubic/executor.py
@@ -1,8 +1,15 @@
 import subprocess
+from typing import Text
+
+from kubic.command import KubicCommand
+from kubic.runnable import KubicRunnable
 
 
-class KubicExecutor(object):
+class KubicExecutor(KubicRunnable):
     KUBECTL = 'kubectl'
+
+    def run(self, option: KubicCommand) -> Text:
+        return ''
 
     def get_kubectl_config(self): 
         config = subprocess.check_output([self.__class__.KUBECTL, 'config', 'view'])
