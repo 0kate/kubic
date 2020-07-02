195cb1b7ac0c8e86b26f6668e3d6cdb587ad6c8a
0K4T3 <o.keito317@gmail.com>
2020-07-03 01:25:46 +0900

    delete invalid test

--- tests/test_kubic_executor.py
+++ tests/test_kubic_executor.py
@@ -7,10 +7,3 @@ from kubic.executor import KubicExecutor
 
 class TestKubicExecutor(TestCase):
     TEST_DATA = KubicCommand('get', 'pod')
-
-    def setUp(self):
-        executor = KubicExecutor()
-        self.executor_returned = executor.run(self.__class__.TEST_DATA)
-
-    def test_run_and_return_text(self):
-        self.assertIsInstance(self.executor_returned, Text)
